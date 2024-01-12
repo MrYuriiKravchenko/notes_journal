36eba1fd677216432b774e3e888cb855ed0e3325
Yurii Kravchenko <MisterKravchenko@yandex.ru>
2024-01-12 18:07:40 +0300

    I continue development

---
+++ notes_journal/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'notes_journal'
+urlpatterns = [
+    # Домашняя страница
+    path('', views.index, name='index'),
+    # Страница со списком всех тем
+    path('topics/', views.topics, name='topics'),
+    # Страница с подробной информации по отдельной теме
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # Страница для добавления новой темы
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # Страница для добавления новой записи
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+]
--- notes_journal/views.py
+++ notes_journal/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Topic
+from .forms import TopicForm, EntryForm
 
-# Create your views here.
+
+def index(request):
+    # Домашняя страница notes_journal
+    return render(request, 'notes_journal/index.html')
+
+
+def topics(request):
+    # Показать все темы
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'notes_journal/topics.html', context)
+
+
+def topic(request, topic_id):
+    # Выводит одну тему и все ее записи
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'notes_journal/topic.html', context)
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('notes_journal:topics')
+
+    context = {'form': form}
+    return render(request, 'notes_journal/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    # Добавляет новую запись по конкретной теме
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('notes_journal:topic', topic_id=topic_id)
+
+    context = {'topic': topic, 'form': form}
+    return render(request, 'notes_journal/new_entry.html', context)

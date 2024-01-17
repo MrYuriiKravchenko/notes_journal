0dfd2f32cbc7634059a9025a8ca5d7ab8cb60607
Yurii Kravchenko <MisterKrava@yandex.ru>
2024-01-17 11:12:31 +0300

    new change now

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
-# Определяет схемы urls для пользователей
-
 from django.urls import path, include
+
+from . import views
 
 app_name = 'accounts'
 urlpatterns = [
     # Включить urls авторизации автоматически
     path('', include('django.contrib.auth.urls')),
+    # Страница регистрации
+    path('register/', views.register, name='register'),
 ]

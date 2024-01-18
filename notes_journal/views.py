5f926301193078bfd4e039b1974aa3121167b5d0
Yurii Kravchenko <MisterKrava@yandex.ru>
2024-01-18 15:04:02 +0300

    save change

--- notes_journal/views.py
+++ notes_journal/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
+
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
@@ -12,7 +14,7 @@ def index(request):
 @login_required
 def topics(request):
     # Показать все темы
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'notes_journal/topics.html', context)
 
@@ -21,6 +23,9 @@ def topics(request):
 def topic(request, topic_id):
     # Выводит одну тему и все ее записи
     topic = Topic.objects.get(id=topic_id)
+    # Проверка того, что тема принадлежит текущему пользователю
+    check_topic_owner(topic, request.user)
+
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'notes_journal/topic.html', context)
@@ -33,7 +38,9 @@ def new_topic(request):
     else:
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('notes_journal:topics')
 
     context = {'form': form}
@@ -44,6 +51,7 @@ def new_topic(request):
 def new_entry(request, topic_id):
     # Добавляет новую запись по конкретной теме
     topic = Topic.objects.get(id=topic_id)
+    check_topic_owner(topic, request.user)
 
     if request.method != 'POST':
         form = EntryForm()
@@ -63,6 +71,7 @@ def new_entry(request, topic_id):
 def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    check_topic_owner(topic, request.user)
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
@@ -74,3 +83,8 @@ def edit_entry(request, entry_id):
 
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'notes_journal/edit_entry.html', context)
+
+
+def check_topic_owner(topic, user):
+    if topic.owner != user:
+        raise Http404
